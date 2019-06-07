d6de3e14421f3a70ea4c6883c62fbea742546bab
Alex Mekelburg <alex@mekware.com>
2019-06-07 15:37:55 -0400

    setup sample

---
+++ Hotels in West Hollywood near Beverly Hills _ Hotel 850 SVB_files/continuous-l-box-js.js
@@ -0,0 +1,17 @@
+/*
+ * FancyBox - jQuery Plugin
+ * Simple and fancy lightbox alternative
+ *
+ * Examples and documentation at: http://fancybox.net
+ *
+ * Copyright (c) 2008 - 2010 Janis Skarnelis
+ * Modified 2012 Nikola Vidoni (mobile ready, CSS3 optimizations, dropped IE6-7 support for shadows & opacity)
+ * That said, it is hardly a one-person project. Many people have submitted bugs, code, and offered their advice freely. Their support is greatly appreciated.
+ *
+ * Version: 1.3.6 (22/11/2012)
+ * Requires: jQuery v1.3+
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
